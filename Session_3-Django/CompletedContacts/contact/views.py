072250b41e14e2c9135da956193a0ae36096d691
Rakshith <hehaichi@gmail.com>
2017-01-19 14:44:36 +0530

    Web-app Structure ready

---
+++ Session_3-Django/CompletedContacts/contact/views.py
@@ -0,0 +1,18 @@
+from .models import Student
+from django.shortcuts import render, get_object_or_404
+
+
+
+# Create your views here.
+
+def home(request):
+
+    students = Student.objects.all()
+    context = {'students':students}
+    return render(request, 'contact/home.html',context=context)
+
+def detail(request,studentid):
+    student = get_object_or_404(Student,pk=studentid)
+
+    context = {'student':student}
+    return render(request, 'contact/details.html',context=context)
